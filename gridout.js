ef4e3798fac93948a7d7ff00647bd02c7adad876
Ryan Birmingham <birm@rbirm.us>
2017-02-15 10:53:30 -0500

    add class

--- gridout.js
+++ gridout.js
@@ -9,24 +9,44 @@
 *@param {int} y - number of grids to draw in the y dimension
 *@param {float} margin - ratio of margin size to grid size (between 0 and 1)
 */
-function grid(x, y, margin){
-  Empty
-  // put in a square div
-  var svg = "<svg style='width:100%;height:100%;'>"
-  // determine sizes in percent
-  var outer_height = 100*(1./x);
-  var inner_height = (1 - margin)* outer_height;
-  var offset = (margin * outer_height)/2;
-  for each in x{
-    // draw top margin box
+/**
+ *A GridOut Object
+ *@constructor
+ *@param {int} x - number of grids to draw in the x dimension
+ *@param {int} y - number of grids to draw in the y dimension
+ *@param {float} margin - ratio of margin size to grid size (between 0 and 1)
+ */
+class GridOut{
+  constructor (x, y, margin){
+    this.x = x;
+    this.y = y;
+    this.margin = margin;
   }
-  for each in y{
+
+  /** Generate a SVG from x, y, and margin sizes.
+  */
+  function grid(){
+    var x = this.x;
+    var y = this.y;
+    var margin = this.margin;
+    // put in a square div
+    var svg = "<svg style='width:100%;height:100%;'>"
+    // determine sizes in percent
+    var outer_height = 100*(1./x);
+    var inner_height = (1 - margin)* outer_height;
+    var offset = (margin * outer_height)/2;
     for each in x{
-      // draw left margin box
-      // draw bottom margin box
+      // draw top margin box
     }
-    // draw right margin box
+    for each in y{
+      for each in x{
+        // draw left margin box
+        // draw bottom margin box
+      }
+      // draw right margin box
+    }
+    svg = svg + "</svg>"
+    return svg;
   }
-  svg = svg + "</svg>"
-  return svg;
+
 }
