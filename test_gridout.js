8d17720ced30ccf461b588696b31b0302dde787c
Ryan Birmingham <birm@rbirm.us>
2017-02-15 18:01:57 -0500

    added document for unit tests

--- test_gridout.js
+++ test_gridout.js
@@ -7,6 +7,25 @@
 var test = require('tape');
 var gridout = require("./gridout");
 
+var { jsdom } = require('jsdom');
+
+function createDocument() {
+  const document = jsdom(undefined);
+  const window = document.defaultView;
+  global.document = document;
+  global.window = window;
+
+  Object.keys(window).forEach((key) => {
+    if (!(key in global)) {
+      global[key] = window[key];
+    }
+  });
+
+  return document;
+}
+
+document = createDocument();
+
 test( 'initialization test', function(t) {
     t.plan(1);
 
@@ -15,18 +34,18 @@ test( 'initialization test', function(t) {
     }, '*', "new GridOut() construction");
 });
 
-test( 'unit tests', function(t) {
-    t.plan(1);
 
-    /**
+test( 'unit tests', function(t) {
+    t.plan(2);
+
     t.doesNotThrow( function() {
       var a = new gridout(5,5,0.1,200);
       a.draw()
     }, '*', "GridOut() svg drawing");
-    */
 
     t.doesNotThrow( function() {
       var a = new gridout(5,5,0.1,200);
+      a.draw()
       var b = gridout.load(a.get_json());
     }, '*', "GridOut() json input and output");
 });
