887a032c54c58343fb92dd482ec8e0c66be17eaf
Ryan Birmingham <birm@rbirm.us>
2017-02-15 17:46:17 -0500

    remove missing doc test

--- test_gridout.js
+++ test_gridout.js
@@ -11,20 +11,22 @@ test( 'initialization test', function(t) {
     t.plan(1);
 
     t.doesNotThrow( function() {
-        basic = new gridout(5, 5, 0.1, 200);
+        var basic = new gridout(5, 5, 0.1, 200);
     }, '*', "new GridOut() construction");
 });
 
 test( 'unit tests', function(t) {
     t.plan(1);
 
+    /**
     t.doesNotThrow( function() {
-      a = new gridout(5,5,0.1,200);
+      var a = new gridout(5,5,0.1,200);
       a.draw()
     }, '*', "GridOut() svg drawing");
+    */
 
     t.doesNotThrow( function() {
-      a = new gridout(5,5,0.1,200);
-      b. gridout.load(a.get_json());
+      var a = new gridout(5,5,0.1,200);
+      var b = gridout.load(a.get_json());
     }, '*', "GridOut() json input and output");
 });
