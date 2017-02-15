22f27ae5e209e5f5f721806a5d22340b766962ac
Ryan Birmingham <birm@rbirm.us>
2017-02-15 11:02:38 -0500

    add basic test

--- test_gridout.js
+++ test_gridout.js
@@ -3,3 +3,14 @@
  *  @author birm@rbirm.us (Ryan Birmingham)
  *  @license Copyright 2017 Ryan Birmingham.
  */
+
+var test = require('tape');
+var GridOut = require("./GridOut");
+
+test( 'initialization tests', function(t) {
+    t.plan(1);
+
+    t.doesNotThrow( function() {
+        onetwothreefour = new Gridout(5, 5, 0.1, 200);
+    }, '*', "new GridOut() construction");
+});
