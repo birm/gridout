750c992bf3294fc169d1b1c4211e91f871eecc6f
Ryan Birmingham <birm@rbirm.us>
2017-02-15 17:40:18 -0500

    add json and draw units

--- test_gridout.js
+++ test_gridout.js
@@ -7,10 +7,24 @@
 var test = require('tape');
 var gridout = require("./gridout");
 
-test( 'initialization tests', function(t) {
+test( 'initialization test', function(t) {
     t.plan(1);
 
     t.doesNotThrow( function() {
         basic = new gridout(5, 5, 0.1, 200);
     }, '*', "new GridOut() construction");
+});
+
+test( 'unit tests', function(t) {
+    t.plan(1);
+
+    t.doesNotThrow( function() {
+      a = new gridout(5,5,0.1,200);
+      a.draw()
+    }, '*', "GridOut() svg drawing");
+
+    t.doesNotThrow( function() {
+      a = new gridout(5,5,0.1,200);
+      b. gridout.load(a.get_json());
+    }, '*', "GridOut() json input and output");
 });
